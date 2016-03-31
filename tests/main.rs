4c6b076504374a9c0cd0ca323bdb8b51bfd60856
Steven Allen <steven@stebalien.com>
2016-03-31 18:24:56 -0400

    Make tests easier to debug

--- tests/main.rs
+++ tests/main.rs
@@ -1,47 +1,29 @@
-#![feature(question_mark)]
-
 extern crate ipfs_api as ipfs;
-
-macro_rules! unwrap {
-    (@ $b:block) => {
-            (|| -> ::std::result::Result<(), Box<::std::error::Error>> {
-                $b
-                Ok(())
-            })().unwrap()
-    };
-    ($($tok:tt)*) => {
-        unwrap!(@ {$($tok)*});
-    };
-}
 
 #[test]
 fn get_object() {
-    unwrap! {
-        let obj = ipfs::object::Object {
-            data: b"testing"[..].to_owned(),
-            links: vec![],
-        };
-        let obj = obj.commit()?;
+    let obj = ipfs::object::Object {
+        data: b"testing"[..].to_owned(),
+        links: vec![],
+    };
+    let obj = obj.commit().unwrap();
 
-        let obj2 = ipfs::object::get(obj.hash())?;
-        assert_eq!(obj, obj2);
-        assert_eq!(&*obj, &*obj2);
-        assert_eq!(obj.data, b"testing");
-        assert_eq!(obj2.data, b"testing");
-    }
+    let obj2 = ipfs::object::get(obj.hash()).unwrap();
+    assert_eq!(obj, obj2);
+    assert_eq!(&*obj, &*obj2);
+    assert_eq!(obj.data, b"testing");
+    assert_eq!(obj2.data, b"testing");
 }
 
 #[test]
 fn publish() {
-    unwrap! {
-        let obj = ipfs::object::Object {
-            data: b"testing"[..].to_owned(),
-            links: vec![],
-        };
-        let obj = obj.commit()?;
-        ipfs::name::publish(&obj);
-        // TODO: lookup my name.
-        let r = ipfs::object::lookup("/ipns/Qme6Q6RCZ7GKmpcuzKDqEtZrisygpTnyneq1N8zchdgWYq")?;
-        assert_eq!(*obj.reference(), r);
-    }
+    let obj = ipfs::object::Object {
+        data: b"testing"[..].to_owned(),
+        links: vec![],
+    };
+    let obj = obj.commit().unwrap();
+    ipfs::name::publish(&obj).unwrap();
+    // TODO: lookup my name.
+    let r = ipfs::object::lookup("/ipns/Qme6Q6RCZ7GKmpcuzKDqEtZrisygpTnyneq1N8zchdgWYq").unwrap();
+    assert_eq!(*obj.reference(), r);
 }
